fcae70c8bc3881e341d940a3918a84fca1cb614e
rhogotargaryen <dhaller.personal@gmail.com>
2019-05-19 23:47:10 -0700

    succesfully added buttont to dom, working on making text on button

--- ttt.js
+++ ttt.js
@@ -1,1 +1,5 @@
-WIN_COMBOS = [[1, 2, 3,], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]
+WIN_COMBOS = [[1, 2, 3,], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]]
+
+let btn = document.createElement("BUTTON");
+btn.innerHTML("TEST_BUTTON")
+document.body.appendChild(btn)
