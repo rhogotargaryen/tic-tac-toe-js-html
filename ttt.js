c3d3cabd60ed57164f0f6dec0cc4218e4024f151
rhogotargaryen <dhaller.personal@gmail.com>
2019-05-19 23:50:24 -0700

    succsesfully captured added element to dom

--- ttt.js
+++ ttt.js
@@ -1,5 +1,12 @@
 WIN_COMBOS = [[1, 2, 3,], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]]
 
+let callBack = (e) => {
+    e.preventDefault()
+    alert('test')
+}
+
 let btn = document.createElement("BUTTON");
-btn.innerHTML= "TEST_BUTTON"
+btn.innerHTML = "TEST_BUTTON"
+btn.addEventListener('click', callBack)
 document.body.appendChild(btn)
+
